e6bd0f356b6ea1f1bf9b550c5d3e04a65970abaa
BLZXB5 <97624291+BLXB567@users.noreply.github.com>
2022-02-17 19:40:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -372,7 +372,7 @@ const checkRegisteredUser = (sender) => {
     const totalhit = JSON.parse(fs.readFileSync("./lib/totalcmd.json"))[0]
       .totalcmd;
 const daftar1 = `Hi ${pushname} ${ucapanWaktu} \n\nBefore Using the Bot Verify First `
-       const daftar2 = '```Click the button below to verify bro or type #verify\nRenge ~Bot```'
+       const daftar2 = '```Click the button below to verify bro or type #verify\nMayumi DVT```'
        const daftar3 = [
           {
             buttonId: `verify`,
